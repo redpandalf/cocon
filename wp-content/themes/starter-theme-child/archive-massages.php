aa824c19e108e6bdd22bf79c12d9c9cf170279fb
asavva <asavva@redpandalf>
2023-01-12 22:43:45 +0100

    blop

--- wp-content/themes/starter-theme-child/archive-massages.php
+++
@@ -1,9 +0,0 @@
-<?php
-    /*
-     Template Name: Mes Messages
-    */
-  $context = Timber::context();
-
-  Timber::render( array( 'page-massages.twig', 'page.twig' ), $context );
- 
-  var_dump('toto');
