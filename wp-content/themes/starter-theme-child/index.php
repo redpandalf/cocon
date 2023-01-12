18a701b3ed7426285fd770d1b8e6f79edc7b64a1
asavva <asavva@redpandalf>
2023-01-12 23:10:16 +0100

    update

--- wp-content/themes/starter-theme-child/index.php
+++ wp-content/themes/starter-theme-child/index.php
@@ -16,7 +16,7 @@
 $context          = Timber::context();
 $context['posts'] = new Timber\PostQuery();
 $context['foo']   = 'bar';
-$templates        = array( 'index.twig' );
+$templates        = array( 'front-page.twig' );
 if ( is_home() ) {
 	array_unshift( $templates, 'front-page.twig', 'home.twig' );
 }
