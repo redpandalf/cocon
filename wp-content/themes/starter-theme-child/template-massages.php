eb3a10cb0bc90fd5fef0d3c9784f94f46b26ccad
asavva <asavva@redpandalf>
2023-01-12 22:13:39 +0100

    update

--- wp-content/themes/starter-theme-child/template-massages.php
+++ wp-content/themes/starter-theme-child/template-massages.php
@@ -4,7 +4,4 @@
 */
 
   $context = Timber::context();
-
-  $timber_post     = new Timber\Post();
-  $context['post'] = $timber_post;
   Timber::render( array( 'page-massages.twig', 'page.twig' ), $context );
