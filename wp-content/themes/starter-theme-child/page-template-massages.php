e40be122602030ab3811d39e512e69d33a0e2ec4
asavva <asavva@redpandalf>
2023-01-12 22:11:00 +0100

    update

--- wp-content/themes/starter-theme-child/page-template-massages.php
+++ wp-content/themes/starter-theme-child/page-template-massages.php
@@ -1,10 +1,8 @@
 <?php
-/**
- * Template Name: Mes Messages
- * 
- */
-$context = Timber::context();
+  /* Template Name: Pages service */
+  
+  $context = Timber::context();
 
-$timber_post     = new Timber\Post();
-$context['post'] = $timber_post;
-Timber::render( array( 'page-massages.twig', 'page.twig' ), $context );
+  $timber_post     = new Timber\Post();
+  $context['post'] = $timber_post;
+  Timber::render( array( 'page-massages.twig', 'page.twig' ), $context );
