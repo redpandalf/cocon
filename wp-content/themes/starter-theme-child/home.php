d7454564c6746ab5643f76bd669081f35a935085
asavva <asavva@redpandalf>
2023-01-12 23:31:38 +0100

    update

--- wp-content/themes/starter-theme-child/home.php
+++ wp-content/themes/starter-theme-child/home.php
@@ -1,16 +1,12 @@
 <?php
 /**
- * The main template file
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists
+ * Template Name: Page d'accueil
  *
- * Methods for TimberHelper can be found in the /lib sub-directory
+ * Displays the Business Template of the theme.
  *
  * @package  WordPress
  * @subpackage  Timber
- * @since   Timber 0.1
+ * @since    Timber 0.1
  */
 
 $context = Timber::get_context();
