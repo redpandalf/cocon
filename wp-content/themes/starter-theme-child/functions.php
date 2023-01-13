288c781c32d412adfe9a52d486ff22f3b639d2c3
asavva <asavva@redpandalf>
2023-01-13 15:44:16 +0100

    update

--- wp-content/themes/starter-theme-child/functions.php
+++ wp-content/themes/starter-theme-child/functions.php
@@ -84,8 +84,12 @@ class StarterSite extends Timber\Site {
 		$context['foo']   = 'bar';
 		$context['stuff'] = 'I am a value set in your functions.php file';
 		$context['notes'] = 'These values are available everytime you call Timber::context();';
-		$context['menu']  = new Timber\Menu();
+		//$context['menu']  = new Timber\Menu();
 		$context['site']  = $this;
+
+		$context['primary_menu']  = new Timber\Menu('Main Menu');
+    $context['footer_menu'] = new Timber\Menu('Footer Menu');
+
 		return $context;
 	}
 
