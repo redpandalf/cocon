411ed721315cbebed252d9e93cbcd8496d64414a
asavva <asavva@redpandalf>
2023-01-13 17:04:07 +0100

    update

--- wp-content/themes/starter-theme-child/functions.php
+++ wp-content/themes/starter-theme-child/functions.php
@@ -172,7 +172,12 @@ class StarterSite extends Timber\Site {
 		return $twig;
 	}
 
+	add_filter( 'timber_context', 'mytheme_timber_context'  );
 
+	function mytheme_timber_context( $context ) {
+			$context['options'] = get_fields('options');
+			return $context;
+	}
 }
 
 new StarterSite();
