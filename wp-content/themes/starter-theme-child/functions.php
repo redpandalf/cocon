e1f03dd3e70b9ba1fb5cdaac97d361a155efe373
asavva <asavva@redpandalf>
2023-01-13 18:21:28 +0100

    update

--- wp-content/themes/starter-theme-child/functions.php
+++ wp-content/themes/starter-theme-child/functions.php
@@ -191,12 +191,15 @@ function cocon_widgets_init() {
 	register_sidebar(array(
 		'name' 						=> 'Footer : Block Contact',
 		'id'							=> 'footer_block_contact',
+		'description'    => '',
+		'class'          => '',
 		'before_widget' 	=> '<div class="block-container">',
 		'after_widget' 		=> '</div>',
 		'before_title' 		=> '<p class="h3 title">',
 		'after_title' 		=> '</p>',
 		'before_sidebar'	=> '',
 		'after_sidebar' 	=> '',
+		'show_in_rest'   => false,
 	));
 
 }
