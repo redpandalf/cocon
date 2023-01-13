2cf98815ced15cdee7124250a2d0a282b7b7d89d
asavva <asavva@redpandalf>
2023-01-13 17:25:08 +0100

    update

--- wp-content/themes/starter-theme-child/functions.php
+++ wp-content/themes/starter-theme-child/functions.php
@@ -90,6 +90,32 @@ class StarterSite extends Timber\Site {
 		$context['main_menu']  = new Timber\Menu('Main Menu');
     $context['footer_menu'] = new Timber\Menu('Footer Menu');
 
+		
+		if( function_exists('acf_add_options_page') ) {
+				
+			acf_add_options_page(array(
+					'page_title'    => 'Theme General Settings',
+					'menu_title'    => 'Theme Settings',
+					'menu_slug'     => 'theme-general-settings',
+					'capability'    => 'edit_posts',
+					'redirect'      => false
+			));
+			
+			acf_add_options_sub_page(array(
+					'page_title'    => 'Theme Header Settings',
+					'menu_title'    => 'Header',
+					'parent_slug'   => 'theme-general-settings',
+			));
+			
+			acf_add_options_sub_page(array(
+					'page_title'    => 'Theme Footer Settings',
+					'menu_title'    => 'Footer',
+					'parent_slug'   => 'theme-general-settings',
+			));
+			
+		}
+
+
 		return $context;
 	}
 
@@ -175,27 +201,3 @@ class StarterSite extends Timber\Site {
 }
 
 new StarterSite();
-
-if( function_exists('acf_add_options_page') ) {
-    
-	acf_add_options_page(array(
-			'page_title'    => 'Theme General Settings',
-			'menu_title'    => 'Theme Settings',
-			'menu_slug'     => 'theme-general-settings',
-			'capability'    => 'edit_posts',
-			'redirect'      => false
-	));
-	
-	acf_add_options_sub_page(array(
-			'page_title'    => 'Theme Header Settings',
-			'menu_title'    => 'Header',
-			'parent_slug'   => 'theme-general-settings',
-	));
-	
-	acf_add_options_sub_page(array(
-			'page_title'    => 'Theme Footer Settings',
-			'menu_title'    => 'Footer',
-			'parent_slug'   => 'theme-general-settings',
-	));
-	
-}
