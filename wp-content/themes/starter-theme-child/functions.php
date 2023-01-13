27e675cbfe51c96c8efdaf899fe36a890a6129cb
asavva <asavva@redpandalf>
2023-01-13 17:35:30 +0100

    update

--- wp-content/themes/starter-theme-child/functions.php
+++ wp-content/themes/starter-theme-child/functions.php
@@ -91,6 +91,13 @@ class StarterSite extends Timber\Site {
 		$context['main_menu']  = new Timber\Menu('Main Menu');
     $context['footer_menu'] = new Timber\Menu('Footer Menu');
 
+		
+add_filter( 'init', 'mytheme_timber_context'  );
+
+function mytheme_timber_context( $context ) {
+    $context['options'] = get_fields('option');
+    return $context;
+}
 
 		return $context;
 	}
@@ -174,13 +181,6 @@ class StarterSite extends Timber\Site {
 		return $twig;
 	}
 
-}
-
-add_filter( 'init', 'mytheme_timber_context'  );
-
-function mytheme_timber_context( $context ) {
-    $context['options'] = get_fields('option');
-    return $context;
 }
 
 new StarterSite();
