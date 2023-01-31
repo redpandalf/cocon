4c4b5b82dadeef1dd468d543aa844831b7f8c71b
asavva <savva.al@gmail.com>
2023-01-31 12:22:02 +0100

    update

--- wp-content/themes/starter-theme-child/functions.php
+++ wp-content/themes/starter-theme-child/functions.php
@@ -206,15 +206,6 @@ class StarterSite extends Timber\Site {
 
 new StarterSite();
 
-// Authorize SVG upload
-function add_file_types_to_uploads($file_types){
-	$new_filetypes = array();
-	$new_filetypes['svg'] = 'image/svg+xml';
-	$file_types = array_merge($file_types, $new_filetypes );
-	return $file_types;
-}
-add_filter('upload_mimes', 'add_file_types_to_uploads');
-
 
 // Widgets list
 function cocon_widgets_init() {
