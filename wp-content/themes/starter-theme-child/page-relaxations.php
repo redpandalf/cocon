049b9c36070ad2323d08a9321e2768127c98a86a
asavva <savva.al@gmail.com>
2023-02-02 17:29:08 +0100

    update

--- wp-content/themes/starter-theme-child/page-relaxations.php
+++ wp-content/themes/starter-theme-child/page-relaxations.php
@@ -13,4 +13,4 @@ $context = Timber::context();
 
 $timber_post     = new Timber\Post();
 $context['post'] = $timber_post;
-Timber::render( array( 'page-relaxations.twig', 'page.twig' ), $context );
+Timber::render( array( 'page-massages.twig', 'page.twig' ), $context );
