141af67b740f7984a17318e11739cd61ccc60189
asavva <savva.al@gmail.com>
2023-02-04 13:57:59 +0100

    update

--- wp-content/themes/starter-theme-child/functions.php
+++ wp-content/themes/starter-theme-child/functions.php
@@ -292,5 +292,5 @@ function cocon_widgets_init() {
 // Remove auto p from Contact Form 7 shortcode output
 add_filter('wpcf7_autop_or_not', 'wpcf7_autop_return_false');
 function wpcf7_autop_return_false() {
-	//return false;
+	return false;
 }
