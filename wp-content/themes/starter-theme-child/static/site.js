4bbcec56652d4cf9e652e3c7cbd582f2633c4449
asavva <savva.al@gmail.com>
2023-02-10 10:30:06 +0100

    update

--- wp-content/themes/starter-theme-child/static/site.js
+++ wp-content/themes/starter-theme-child/static/site.js
@@ -5,12 +5,12 @@ $( document ).ready( function( $ ) {
   $(".all-clickable").each(function () {
     var t = $(this).find("a:first-of-type").attr("href");
     
-    $(this).on("click", function () {
+    $(this).on("click", function (e) {
       console.log(t);
 
       var i = $(this).find("input");
       r = $(this).find(".no-all-clickable");
-      
+
       t != typeof inputOfCar ? $(event.target).closest(i).length || e(t) : (void 0 !== r && $(event.target).closest(r).length) || e(t);
       /*var i = $(this).find("input"),
           r = $(this).find(".no-all-clickable");
