0653113e59970f005b32f5f3894b79ef25882560
asavva <savva.al@gmail.com>
2023-02-10 10:23:41 +0100

    update

--- wp-content/themes/starter-theme-child/static/site.js
+++ wp-content/themes/starter-theme-child/static/site.js
@@ -3,7 +3,7 @@ jQuery( document ).ready( function( $ ) {
   // Your JavaScript goes here
   alert('Coucou');
 
-  /*
+
   $(".all-clickable").each(function () {
     var t = $(this).find("a:first-of-type").attr("href");
     $(this).on("click", function () {
@@ -12,5 +12,5 @@ jQuery( document ).ready( function( $ ) {
       "undefined" != typeof inputOfCar ? $(event.target).closest(i).length || e(t) : (void 0 !== r && $(event.target).closest(r).length) || e(t);
     });
   });
-  */
+  
 });
